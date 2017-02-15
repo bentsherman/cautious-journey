a439c31c4ec9c221d01dbfcf72725d2e316366fe
Ben Shealy <bentshermann@gmail.com>
2017-02-15 15:16:49 -0500

    Added code to create binary tree and debug-print Huffman codes

--- compress.c
+++ compress.c
@@ -3,18 +3,21 @@
  *
  * Implementation of Huffman compression.
  */
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct node {
     char symbol;
     int weight;
-} queue_entry_t;
+    struct node *left;
+    struct node *right;
+} node_t;
 
 typedef struct {
     int size;
     int num_entries;
-    queue_entry_t *entries;
+    node_t **entries;
 } queue_t;
 
 /**
@@ -27,7 +30,7 @@ queue_t * queue_construct(int size)
     queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
     queue->size = size;
     queue->num_entries = 0;
-    queue->entries = (queue_entry_t *)malloc(size * sizeof(queue_entry_t));
+    queue->entries = (node_t **)malloc(size * sizeof(node_t *));
 
     return queue;
 }
@@ -44,26 +47,72 @@ void queue_destruct(queue_t *queue)
 }
 
 /**
+ * Get the length of a priority queue.
+ *
+ * @param queue
+ * @return length of queue
+ */
+int queue_length(queue_t *queue)
+{
+    return queue->num_entries;
+}
+
+/**
  * Push an entry into a priority queue.
  *
  * @param queue
- * @param symbol
- * @param weight
+ * @param entry
  */
-void queue_push(queue_t *queue, char symbol, int weight)
+void queue_push(queue_t *queue, node_t *entry)
 {
     // increase queue size if queue is full
     if ( queue->num_entries == queue->size ) {
         queue->size *= 2;
-        queue->entries = (queue_entry_t *)realloc(queue->entries, queue->size * sizeof(queue_entry_t));
+        queue->entries = (node_t **)realloc(queue->entries, queue->size * sizeof(node_t *));
     }
 
     // insert entry
-    queue_entry_t *entry = &queue->entries[queue->num_entries];
-    entry->symbol = symbol;
-    entry->weight = weight;
-
+    queue->entries[queue->num_entries] = entry;
     queue->num_entries++;
+}
+
+/**
+ * Remove the entry of highest priority from a queue.
+ *
+ * The entry of highest priority is the entry with the
+ * lowest weight.
+ *
+ * @param queue
+ * @return entry of highest priority, or NULL if queue is empty
+ */
+node_t * queue_remove(queue_t *queue)
+{
+    if ( queue->num_entries == 0 ) {
+        return NULL;
+    }
+
+    // find entry with the lowest weight
+    int min_index = 0;
+    node_t *min = queue->entries[0];
+
+    int i;
+    for ( i = 1; i < queue->num_entries; i++ ) {
+        node_t *entry = queue->entries[i];
+
+        if ( min->weight > entry->weight ) {
+            min_index = i;
+            min = entry;
+        }
+    }
+
+    // remove the entry from the queue
+    for ( i = min_index; i + 1 < queue->num_entries; i++ ) {
+        queue->entries[i] = queue->entries[i + 1];
+    }
+
+    queue->num_entries--;
+
+    return min;
 }
 
 /**
@@ -75,9 +124,60 @@ void queue_debug_print(queue_t *queue)
 {
     int i;
     for ( i = 0; i < queue->num_entries; i++ ) {
-        queue_entry_t *entry = &queue->entries[i];
+        node_t *entry = queue->entries[i];
 
-        printf("%hhu  %d\n", entry->symbol, entry->weight);
+        printf("%3hhu  %8d\n", entry->symbol, entry->weight);
+    }
+    putchar('\n');
+}
+
+/**
+ * Destruct a binary tree.
+ *
+ * @param root
+ */
+void tree_destruct(node_t *root)
+{
+    if ( root == NULL ) {
+        return;
+    }
+
+    free(root->left);
+    free(root->right);
+    free(root);
+}
+
+/**
+ * Print the Huffman code for each leaf node in a binary tree.
+ *
+ * As a common convention, bit '0' represents following the
+ * left child and bit '1' represents following the right child.
+ *
+ * @param root
+ * @param len
+ * @param code
+ */
+void huffman_print(node_t *root, int len, unsigned int code)
+{
+    assert((root->left == NULL) == (root->right == NULL));
+
+    if ( root->left == NULL && root->right == NULL ) {
+        printf("%3hhu  %8d  %2d  ", root->symbol, root->weight, len);
+
+        int i;
+        for ( i = 0; i < len; i++ ) {
+            char digit = (((code >> (len - i - 1)) & 0x1) == 0x1)
+                ? '1'
+                : '0';
+
+            putchar(digit);
+        }
+
+        putchar('\n');
+    }
+    else {
+        huffman_print(root->left, len + 1, (code << 1) | 0x0);
+        huffman_print(root->right, len + 1, (code << 1) | 0x1);
     }
 }
 
@@ -116,11 +216,39 @@ int main(int argc, char **argv)
         }
 
         if ( weight > 0 ) {
-            queue_push(queue, symbol, weight);
+            node_t *entry = (node_t *)malloc(sizeof(node_t));
+            entry->symbol = symbol;
+            entry->weight = weight;
+            entry->left = NULL;
+            entry->right = NULL;
+
+            queue_push(queue, entry);
         }
     }
 
     queue_debug_print(queue);
+
+    // create binary tree
+    while ( queue_length(queue) > 1 ) {
+        // remove the two nodes of lowest weight
+        node_t *left = queue_remove(queue);
+        node_t *right = queue_remove(queue);
+
+        // create a parent node whose weight is the sum of the children
+        node_t *parent = (node_t *)malloc(sizeof(node_t));
+        parent->weight = left->weight + right->weight;
+        parent->left = left;
+        parent->right = right;
+
+        // add parent node to the queue
+        queue_push(queue, parent);
+    }
+
+    // set the last node as the root node
+    node_t *tree = queue_remove(queue);
+
+    // print codes for the tree
+    huffman_print(tree, 0, 0x00);
 
     // write compressed data to file
     FILE *out = fopen("data.huff", "wb");
@@ -132,6 +260,7 @@ int main(int argc, char **argv)
     // cleanup
     free(data);
     queue_destruct(queue);
+    tree_destruct(tree);
 
     return 0;
 }
